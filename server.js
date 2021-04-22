e9854a7d3e108137957b7f5c1a6cb47b8d7fed1d
16Maarten <176maarten@gmail.com>
2021-04-22 13:16:50 +0200

    different app

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 const express = require('express')
 const app = express()
-const http = require('http')
+const port = 3000
 
-const port = process.env.PORT || 3000
-let result = {
-  'response':'Hello World!',
-  'status' : 'Alles OK!'
-}
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'appliaction/json')
-  res.end(JSON.stringify(result))
+app.get('/', (req, res) => {
+  res.status(200).json('Hello World!')
 })
 
-server.listen(port, () => {
-  console.log(`Server running at http://:${port}/`)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
