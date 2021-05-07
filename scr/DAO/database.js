7d937b3c9b5528c3b346843543835cc14437e7cc
16Maarten <176maarten@gmail.com>
2021-05-07 13:37:21 +0200

    added update functionality

--- scr/DAO/database.js
+++ scr/DAO/database.js
@@ -1,12 +1,12 @@
 let lastInsertedIndex = 1;
 let input = {
   name: "Studentenhuis Breda",
-  StreetName: "Lovensdijkstraat",
-  HouseNumber: "45",
-  Postalcode: "3425FK",
-  residence: "Breda",
+  streetName: "Lovensdijkstraat",
+  houseNumber: "45",
+  postalcode: "3425FK",
+  city: "Breda",
   phoneNumber: "0656341298",
-  id: 0
+  id: 0,
 };
 
 let database = {
@@ -15,30 +15,68 @@ let database = {
 
   get(name, city, callback) {
     let filteredStudenthome = this.db.filter((e) => {
-        return (
-          e.name == name && e.city == city
-          )
-      });
-      if(filteredStudenthome.length > 0){
-        callback(filteredStudenthome, undefined);
-      } else {
-          let err = {
-              message: "Studenthome doesn't exist",
-              errCode: 400
-          }
-          if (name) {
-            err.message += " name: " + name;
-          }
-          if (city) {
-            err.message += " city: " + city;
-          }
-          callback(undefined, err);
-        }
+      return e.name == name && e.city == city;
+    });
+    if (filteredStudenthome.length > 0) {
+      callback(filteredStudenthome, undefined);
+    } else {
+      let err = {
+        message: "Studenthome doesn't exist",
+        errCode: 400,
+      };
+      if (name) {
+        err.message += " name: " + name;
+      }
+      if (city) {
+        err.message += " city: " + city;
+      }
+      callback(undefined, err);
+    }
+  },
+
+  getDetailed(id, callback) {
+    let detailedStudenthome = this.db.filter((e) => {
+      return e.id == id;
+    });
+    if (detailedStudenthome.length > 0) {
+      callback(detailedStudenthome[0], undefined);
+    } else {
+      let err = {
+        message: "Studenthome id doesn't exist",
+        errCode: 400,
+      };
+      if (id) {
+        err.message += " id: " + id;
+      }
+      callback(undefined, err);
+    }
+  },
+
+  delete(id, callback) {
+    let deleteStudenthome = this.db.filter((e) => {
+      return e.id == id;
+    });
+    const index = this.db.indexOf(deleteStudenthome[0]);
+    if (deleteStudenthome.length > 0) {
+      this.db.splice(index, 1);
+      callback(deleteStudenthome[0], undefined);
+    } else {
+      let err = {
+        message: "Studenthome id doesn't exist",
+        errCode: 400,
+      };
+      if (id) {
+        err.message += " id: " + id;
+      }
+      callback(undefined, err);
+    }
   },
 
   add(item, callback) {
     setTimeout(() => {
-      item.id = lastInsertedIndex++;
+      if(!item.id){
+        item.id = lastInsertedIndex++;
+      }
       this.db.push(item);
       callback(item, undefined);
     }, 2000);
--- scr/controllers/studenthomeController.js
+++ scr/controllers/studenthomeController.js
@@ -9,7 +9,7 @@ let controller = {
     logger.info("Studenthome body: " + studenthome);
     db.add(studenthome, (result, err) => {
       if (err) {
-        next(err)
+        next(err);
       }
       if (result) {
         logger.debug(db.db);
@@ -25,15 +25,17 @@ let controller = {
     const city = req.query.city;
     logger.info("Name: " + name + " City: " + city);
     logger.info("Studenthome endpoint called");
-    db.get(name,city, (result, err) => {
+    db.get(name, city, (result, err) => {
       if (err) {
-        next(err)
+        next(err);
       }
       if (result) {
-        let studenthomes = []
-        for(let i = 0; i < result.length; i++) {
-          studenthomes.push({Studenthome: result[i].name, id: result[i].id})
-          logger.debug("Studenthome: "+ result[i].name +" id: " + result[i].id);
+        let studenthomes = [];
+        for (let i = 0; i < result.length; i++) {
+          studenthomes.push({ Studenthome: result[i].name, id: result[i].id });
+          logger.debug(
+            "Studenthome: " + result[i].name + " id: " + result[i].id
+          );
         }
         res.status(200).json({ status: "success", studenthomes });
       }
@@ -41,21 +43,62 @@ let controller = {
   },
 
   // UC-203 Details van studentenhuis
-  getDetailStudenthome(req, res) {
+  getDetailStudenthome(req, res, next) {
+    const id = req.params.homeId;
+    logger.debug(id);
     logger.info("Studenthome/:homeId endpoint called");
-    res.status(200).send(result);
+    db.getDetailed(id, (result, err) => {
+      if (err) {
+        next(err);
+      }
+      if (result) {
+        logger.info(result);
+        res.status(200).send({status: "success",result});
+      }
+    });
   },
 
   // UC-204 Studentenhuis wijzigen
   updateStudenthome(req, res) {
     logger.info("Studenthome/:homeId endpoint called");
-    res.status(200).send(result);
+    const id = req.params.homeId;
+    let studenthome = req.body;
+    logger.debug(id);
+    db.delete(id, (result, err) => {
+      if (err) {
+        next(err);
+      }
+      if (result) {
+        studenthome.id = id
+        db.add(studenthome, (result2, err2) => {
+          if (err2) {
+            next(err2);
+          }
+          if (result2) {
+            logger.debug(db.db);
+            logger.debug("StudentHome added to database");
+            studenthome = result2
+            res.status(200).json({ status: "success", studenthome, message: "updated"});
+          }
+        });
+      }
+    });
   },
 
   // UC-205 Studentenhuis verwijderen
   deleteStudenthome(req, res) {
+    const id = req.params.homeId;
+    logger.debug(id);
     logger.info("Studenthome/:homeId endpoint called");
-    res.status(200).send(result);
+    db.delete(id, (result, err) => {
+      if (err) {
+        next(err);
+      }
+      if (result) {
+        logger.info(result);
+        res.status(200).send({status: "success",result});
+      }
+    });
   },
 
   // UC-206 Gebruiker toevoegen aan studentenhuis
