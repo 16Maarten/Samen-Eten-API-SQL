b7606f37e04fc340094598d0067bdefffc3f9acc
16Maarten <176maarten@gmail.com>
2021-05-14 20:17:08 +0200

    added features to development branch

--- src/routers/mealsRouter.js
+++ src/routers/mealsRouter.js
@@ -4,13 +4,13 @@ const mealsController = require("../controllers/mealsController");
 const authenticationController = require("../controllers/authenticationController");
 
 // UC-301 Maaltijd aanmaken
-router.post("/:homeId/meal",authenticationController.validateToken,authenticationController.validateUser, mealsController.validateStudentHomeMeal, mealsController.createStudenthomeMeal);
+router.post("/:homeId/meal",authenticationController.validateToken, authenticationController.validateUser, mealsController.validateStudentHomeMeal, mealsController.createStudenthomeMeal);
 // UC-302 Maaltijd wijzigen
-router.put("/:homeId/meal/:mealId",authenticationController.validateToken,authenticationController.validateUser, mealsController.validateStudentHomeMeal, mealsController.updateStudenthomeMeal);
+router.put("/:homeId/meal/:mealId",authenticationController.validateToken, authenticationController.validateUser, mealsController.validateStudentHomeMeal, mealsController.updateStudenthomeMeal);
 // UC-303 Lijst van maaltijden opvragen
 router.get("/:homeId/meal", mealsController.getStudenthomeMeals);
 // UC-304 Details van een maaltijd opvragen
 router.get("/:homeId/meal/:mealId", mealsController.getStudenthomeDetailMeal);
 // UC-305 Maaltijd verwijderen
-router.delete("/:homeId/meal/:mealId",authenticationController.validateToken,authenticationController.validateUser, mealsController.deleteStudenthomeMeal);
+router.delete("/:homeId/meal/:mealId",authenticationController.validateToken, authenticationController.validateUser, mealsController.deleteStudenthomeMeal);
 module.exports = router;
